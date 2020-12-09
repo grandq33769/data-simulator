8cdee5380b82ee3044a7d80122a7436ad2ce696e
lhleung <grandq33769@gmail.com>
2020-12-09 09:22:44 +0800

    feat(model): add "Base" model for general function

--- src/model/config.py
+++ src/model/config.py
@@ -1,26 +1,32 @@
 # Standard Library
 from dataclasses import dataclass
-from typing import List
+from typing import Callable, List, Union
 from urllib.parse import ParseResult
 
-from .model import DataModel
+from .model import Base, DataModel
 
 
 @dataclass
-class Authorization:
-    token: str
+class Authorization(Base):
+    token: str = ''
     schema: str = 'Bearer'
 
 
 @dataclass
-class Environment:
-    auth: Authorization
-    db_url: ParseResult
-    endpoint: ParseResult
+class Environment(Base):
+    auth: Union[Authorization, None] = None
+    db_url: Union[ParseResult, None] = None
+    endpoint: Union[ParseResult, None] = None
 
 
 @dataclass
-class Config:
+class Config(Base):
     module: str
     env: Environment
     data: List[DataModel]
+
+
+@dataclass
+class ExecuteType(Base):
+    func: Callable
+    date_format: str
