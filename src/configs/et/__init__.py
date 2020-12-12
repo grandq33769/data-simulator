a83383c408e24f74b23e72160e8dee73a886407b
lhleung <grandq33769@gmail.com>
2020-12-12 19:08:09 +0800

    feat(config/et): add 'carlab' config

---
+++ src/configs/et/__init__.py
@@ -0,0 +1,7 @@
+# Standard Library
+from typing import List
+
+from ...model.et.env import ETConfig
+from .carlab import CONFIG as carlab_config
+
+ET_CONFIG: List[ETConfig] = [carlab_config]
---
+++ src/configs/et/shared.py
@@ -0,0 +1,4 @@
+# Standard Library
+from urllib.parse import urlparse
+
+ENDPOINT = urlparse('https://et.udc-service.io:5000')
