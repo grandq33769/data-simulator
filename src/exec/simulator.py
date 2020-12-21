ef3070a1a1d5e43f0a27977e2ce06c1b1ce0a104
lhleung <grandq33769@gmail.com>
2020-12-21 22:44:17 +0800

    fix(simulator): 1. add detailed logging\n2. add callback args\n3. accept kwargs to callback\n4. update callback kwargs after simulation

--- src/exec/simulator.py
+++ src/exec/simulator.py
@@ -1,3 +1,7 @@
+# Standard Library
+import os
+from typing import Any, Dict
+
 # Third Party Library
 from loguru import logger as log
 
@@ -14,16 +18,31 @@ class SimulationHandler(Base):
     def execute(self):
         for time in range(self.model.quantity):
             simulated_data = self.simulate(time)
-            self.model.callback(simulated_data, self.model, self.env)
+            log.info(
+                f'[{self.model.classname}-Process-{os.getpid()}] '
+                f'Simulated Data: {simulated_data}'
+            )
+            self.model.callback.kwargs.update(
+                {'data': simulated_data, 'model': self.model, 'env': self.env}
+            )
+            self.model.callback.func(**self.model.callback.kwargs)
+            log.info(
+                f'[{self.model.classname}-Process-{os.getpid()}] '
+                f'Callback "{self.model.callback.func.__name__}" Completed.'
+            )
 
     def simulate(self, time: int) -> dict:
-        result = dict()
+        result: Dict[str, Any] = dict()
         for attribute_name in self.model.get_attributes():
             try:
                 attribute = getattr(self.model, attribute_name)
-                attribute.kwargs.update({'time': time})
+                attribute.kwargs.update({'time': time, 'env': self.env})
                 result.update(
-                    {attribute_name: attribute.get(**attribute.kwargs)}
+                    {
+                        attribute_name: attribute.get(
+                            **attribute.kwargs, **result
+                        )
+                    }
                 )
             except Exception as e:
                 log.exception(
