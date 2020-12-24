5e2eadcf254f7e27fbd08fbfce54b7050b71e807
lhleung <grandq33769@gmail.com>
2020-12-24 17:15:52 +0800

    fix(configs/sample): remove mypy wrong checking

--- src/configs/et/shared.py
+++ src/configs/et/shared.py
@@ -1,9 +1,8 @@
 # Standard Library
 import os
-from urllib.parse import urlparse
 
 from ...model.data import Callback
 
-ENDPOINT = urlparse(os.environ.get('ET_HOST', 'http://localhost:5000'))
+ENDPOINT = os.environ.get('ET_HOST', 'http://localhost:5000')
 LOGIN_URL = '/login'
 LOGIN_CALLBACK = Callback(func=lambda response: response['bearer'])
